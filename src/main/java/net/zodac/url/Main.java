bb92bf469eede2b03e22281225fb146eb38ae31c
zodac <arouge110@msn.com>
2026-05-09 19:12:50 +1200

    Making Tomcat port configurable

--- src/main/java/net/zodac/url/Main.java
+++ src/main/java/net/zodac/url/Main.java
@@ -3,24 +3,36 @@ package net.zodac.url;
 import java.io.File;
 
 import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
 import org.apache.catalina.startup.Tomcat;
 
+/**
+ * Main application class.
+ */
 public class Main {
 
-	public static void main(String[] args) throws Exception {
-		final int port = 8080;
+    private static final int TOMCAT_PORT = EnvironmentVariableUtils.getIntOrDefault("TOMCAT_PORT", 8080);
 
-		final Tomcat tomcat = new Tomcat();
-		tomcat.setPort(port);
-		tomcat.getConnector();
+    /**
+     * Main method to start the Tomcat instance.
+     *
+     * @param args application arguments
+     */
+    public static void main(final String[] args) {
+        final Tomcat tomcat = new Tomcat();
+        tomcat.setPort(TOMCAT_PORT);
+        tomcat.getConnector();
 
-		final Context context = tomcat.addContext("", new File(".").getAbsolutePath());
+        final Context context = tomcat.addContext("", new File(".").getAbsolutePath());
+        Tomcat.addServlet(context, "urlShortener", new UrlShortenerServlet());
+        context.addServletMappingDecoded("/*", "urlShortener");
 
-		Tomcat.addServlet(context, "urlShortener", new UrlShortenerServlet());
-		context.addServletMappingDecoded("/*", "urlShortener");
-
-		tomcat.start();
-		System.out.println("Server started on http://localhost:" + port);  // TODO: Logger
-		tomcat.getServer().await();
-	}
+        try {
+            tomcat.start();
+            System.out.println("Server started on http://localhost:" + TOMCAT_PORT);  // TODO: Logger
+            tomcat.getServer().await();
+        } catch (final LifecycleException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
